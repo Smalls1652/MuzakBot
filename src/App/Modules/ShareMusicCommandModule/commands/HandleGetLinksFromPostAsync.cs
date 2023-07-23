4a563e1021e1aaf7d4dff653b632b8af63af75b2
Timothy Small <smalls.online@icloud.com>
2023-07-23 12:03:32 -0400

    [Update] Add message command for parsing messages (#5)
    Adds a message command for parsing links in a message.

---
+++ src/App/Modules/ShareMusicCommandModule/commands/HandleGetLinksFromPostAsync.cs
@@ -0,0 +1,109 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using Discord;
+using Discord.Interactions;
+using MuzakBot.App.Models.Odesli;
+using MuzakBot.App.Services;
+using Microsoft.Extensions.Logging;
+
+namespace MuzakBot.App.Modules;
+
+public partial class ShareMusicCommandModule
+{
+    [MessageCommand(
+        name: "Get music share links"
+    )]
+    private async Task HandleGetLinksFromPostAsync(IMessage message)
+    {
+        Regex linkRegex = new(@"(?'musicLink'(?>https|http):\/\/(?>[A-Za-z0-9\.]+)(?>\/\S*[^\.\s]|))(?> |)", RegexOptions.Multiline);
+
+        await DeferAsync(
+            ephemeral: true
+        );
+
+        _logger.LogInformation("Message content: {messageContent}", message.CleanContent);
+
+        if (!linkRegex.IsMatch(message.CleanContent))
+        {
+            await FollowupAsync(
+                text: "Could not find a link in that post.",
+                ephemeral: true
+            );
+
+            return;
+        }
+
+        MatchCollection linkMatches = linkRegex.Matches(message.CleanContent);
+
+        StringBuilder stringBuilder = new("Results:\n");
+        for (var i = 0; i < linkMatches.Count; i++)
+        {
+            string url = linkMatches[i].Groups["musicLink"].Value;
+
+            MusicEntityItem? musicEntityItem = null;
+            try
+            {
+                musicEntityItem = await _odesliService.GetShareLinksAsync(url);
+
+                if (musicEntityItem is null)
+                {
+                    throw new Exception("No share links found.");
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "No share links found for '{url}'.", url);
+                stringBuilder.AppendLine($"{i + 1}. No share links were found for `{url}`.");
+                continue;
+            }
+
+            PlatformEntityLink? platformEntityLink;
+            try
+            {
+                platformEntityLink = musicEntityItem.LinksByPlatform!["itunes"];
+            }
+            catch
+            {
+                var streamingEntityWithThumbnailUrl = musicEntityItem.EntitiesByUniqueId!.FirstOrDefault(entity => entity.Value.ThumbnailUrl is not null).Value.ApiProvider;
+
+                if (!string.IsNullOrEmpty(streamingEntityWithThumbnailUrl))
+                {
+                    platformEntityLink = musicEntityItem.LinksByPlatform![streamingEntityWithThumbnailUrl];
+                }
+                else
+                {
+                    _logger.LogError("Couldn't get all of the necessary data for '{url}'.", url);
+                    stringBuilder.AppendLine($"{i + 1}. Couldn't get all of the necessary data from Odesli for `{url}`.");
+                    continue;
+                }
+            }
+
+            StreamingEntityItem streamingEntityItem = musicEntityItem.EntitiesByUniqueId![platformEntityLink.EntityUniqueId!];
+            using var albumArtStream = await GetMusicEntityItemAlbumArtAsync(streamingEntityItem);
+
+            var linksComponentBuilder = GenerateMusicShareComponent(musicEntityItem);
+
+            var messageEmbed = new EmbedBuilder()
+                .WithTitle(streamingEntityItem.Title)
+                .WithDescription($"by {streamingEntityItem.ArtistName}")
+                .WithColor(Color.DarkBlue)
+                .WithFooter("(Powered by Songlink/Odesli)");
+
+            await Context.Channel.SendFileAsync(
+                embed: messageEmbed.Build(),
+                stream: albumArtStream,
+                filename: $"{streamingEntityItem.Title}.jpg",
+                components: linksComponentBuilder.Build(),
+                messageReference: new(message.Id),
+                allowedMentions: AllowedMentions.None
+            );
+
+            stringBuilder.AppendLine($"{i + 1}. Successfully got links for `{url}`.");
+        }
+
+        await FollowupAsync(
+            text: stringBuilder.ToString(),
+            ephemeral: true
+        );
+    }
+}
