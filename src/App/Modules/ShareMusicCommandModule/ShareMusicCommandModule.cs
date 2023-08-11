2f30dbb27dc22a5531f2e51e0f712c7d4874ea7e
Timothy Small <smalls.online@icloud.com>
2023-08-11 14:53:53 -0400

    Add MusicBrainzService to constructor

--- src/App/Modules/ShareMusicCommandModule/ShareMusicCommandModule.cs
+++ src/App/Modules/ShareMusicCommandModule/ShareMusicCommandModule.cs
@@ -14,14 +14,16 @@ public partial class ShareMusicCommandModule : InteractionModuleBase
     private readonly IDiscordService _discordService;
     private readonly IOdesliService _odesliService;
     private readonly IItunesApiService _itunesApiService;
+    private readonly IMusicBrainzService _musicBrainzService;
     private readonly IHttpClientFactory _httpClientFactory;
     private readonly ILogger<ShareMusicCommandModule> _logger;
 
-    public ShareMusicCommandModule(IDiscordService discordService, IOdesliService odesliService, IItunesApiService itunesApiService, IHttpClientFactory httpClientFactory, ILogger<ShareMusicCommandModule> logger)
+    public ShareMusicCommandModule(IDiscordService discordService, IOdesliService odesliService, IItunesApiService itunesApiService, IMusicBrainzService musicBrainzService, IHttpClientFactory httpClientFactory, ILogger<ShareMusicCommandModule> logger)
     {
         _discordService = discordService;
         _odesliService = odesliService;
         _itunesApiService = itunesApiService;
+        _musicBrainzService = musicBrainzService;
         _httpClientFactory = httpClientFactory;
         _logger = logger;
     }
