0400d11f6c5d76ad08dc39e58c4b51fa3de19cf2
Timothy Small <smalls.online@icloud.com>
2024-01-03 14:22:44 -0500

    Update MusicBrainzService
    - Implements activity tracing
    - Added code docs
    - Implemented IDisposable
    - Added using statement for created HttpClients

--- src/App/Services/MusicBrainzService/interfaces/IMusicBrainzService.cs
+++ src/App/Services/MusicBrainzService/interfaces/IMusicBrainzService.cs
@@ -2,12 +2,23 @@ using MuzakBot.App.Models.MusicBrainz;
 
 namespace MuzakBot.App.Services;
 
-public interface IMusicBrainzService
+public interface IMusicBrainzService : IDisposable
 {
     Task<MusicBrainzArtistSearchResult?> SearchArtistAsync(string artistName);
+    Task<MusicBrainzArtistSearchResult?> SearchArtistAsync(string artistName, string? parentActivityId);
+
     Task<MusicBrainzRecordingSearchResult?> SearchArtistRecordingsAsync(string artistId, string songName);
+    Task<MusicBrainzRecordingSearchResult?> SearchArtistRecordingsAsync(string artistId, string songName, string? parentActivityId);
+
     Task<MusicBrainzReleaseSearchResult?> SearchArtistReleasesAsync(string artistId, string albumName);
+    Task<MusicBrainzReleaseSearchResult?> SearchArtistReleasesAsync(string artistId, string albumName, string? parentActivityId);
+
     Task<MusicBrainzArtistItem?> LookupArtistAsync(string artistId);
+    Task<MusicBrainzArtistItem?> LookupArtistAsync(string artistId, string? parentActivityId);
+
     Task<MusicBrainzReleaseItem?> LookupReleaseAsync(string releaseId);
+    Task<MusicBrainzReleaseItem?> LookupReleaseAsync(string releaseId, string? parentActivityId);
+
     Task<MusicBrainzRecordingItem?> LookupRecordingAsync(string recordingId);
+    Task<MusicBrainzRecordingItem?> LookupRecordingAsync(string recordingId, string? parentActivityId);
 }
