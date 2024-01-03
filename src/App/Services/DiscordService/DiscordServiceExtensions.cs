08077212a7d2fed7bab8f0bf08c1678670b44b36
Timothy Small <smalls.online@icloud.com>
2024-01-03 16:37:15 -0500

    Add DiscordServiceExtensions

---
+++ src/App/Services/DiscordService/DiscordServiceExtensions.cs
@@ -0,0 +1,45 @@
+using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MuzakBot.App.Services;
+
+/// <summary>
+/// Extension methods for adding Discord service to the service collection.
+/// </summary>
+public static class DiscordServiceExtensions
+{
+    /// <summary>
+    /// Adds the Discord service to the service collection.
+    /// </summary>
+    /// <param name="services">The service collection.</param>
+    /// <param name="configure">An action to configure the Discord service options.</param>
+    /// <returns>The updated service collection.</returns>
+    public static IServiceCollection AddDiscordService(this IServiceCollection services, Action<DiscordServiceOptions> configure)
+    {
+        GatewayIntents gatewayIntents = GatewayIntents.AllUnprivileged - GatewayIntents.GuildInvites - GatewayIntents.GuildScheduledEvents;
+
+#if DEBUG
+        DiscordSocketConfig discordSocketConfig = new()
+        {
+            GatewayIntents = gatewayIntents,
+            UseInteractionSnowflakeDate = false
+        };
+#else
+        DiscordSocketConfig discordSocketConfig = new()
+        {
+            GatewayIntents = gatewayIntents
+        };
+#endif
+
+        services.Configure(configure);
+
+        services.AddSingleton<DiscordSocketClient>(
+            implementationInstance: new(discordSocketConfig)
+        );
+
+        services.AddHostedService<DiscordService>();
+
+        return services;
+    }
+}
