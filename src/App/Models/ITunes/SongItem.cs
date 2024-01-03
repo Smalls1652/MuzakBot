cb8359de4698909f16b0e115a6661976c660c3f1
Timothy Small <smalls.online@icloud.com>
2024-01-02 19:44:01 -0500

    Declare all string props to be nullable

--- src/App/Models/ITunes/SongItem.cs
+++ src/App/Models/ITunes/SongItem.cs
@@ -5,10 +5,10 @@ namespace MuzakBot.App.Models.Itunes;
 public class SongItem : ISongItem
 {
     [JsonPropertyName("wrapperType")]
-    public string WrapperType { get; set; }
+    public string? WrapperType { get; set; }
 
     [JsonPropertyName("kind")]
-    public string Kind { get; set; }
+    public string? Kind { get; set; }
 
     [JsonPropertyName("artistId")]
     public long ArtistId { get; set; }
@@ -20,40 +20,40 @@ public class SongItem : ISongItem
     public long TrackId { get; set; }
 
     [JsonPropertyName("artistName")]
-    public string ArtistName { get; set; }
+    public string? ArtistName { get; set; }
 
     [JsonPropertyName("collectionName")]
-    public string CollectionName { get; set; }
+    public string? CollectionName { get; set; }
 
     [JsonPropertyName("trackName")]
-    public string TrackName { get; set; }
+    public string? TrackName { get; set; }
 
     [JsonPropertyName("collectionCensoredName")]
-    public string CollectionCensoredName { get; set; }
+    public string? CollectionCensoredName { get; set; }
 
     [JsonPropertyName("trackCensoredName")]
-    public string TrackCensoredName { get; set; }
+    public string? TrackCensoredName { get; set; }
 
     [JsonPropertyName("artistViewUrl")]
-    public string ArtistViewUrl { get; set; }
+    public string? ArtistViewUrl { get; set; }
 
     [JsonPropertyName("collectionViewUrl")]
-    public string CollectionViewUrl { get; set; }
+    public string? CollectionViewUrl { get; set; }
 
     [JsonPropertyName("trackViewUrl")]
-    public string TrackViewUrl { get; set; }
+    public string? TrackViewUrl { get; set; }
 
     [JsonPropertyName("previewUrl")]
-    public string PreviewUrl { get; set; }
+    public string? PreviewUrl { get; set; }
 
     [JsonPropertyName("artworkUrl30")]
-    public string ArtworkUrl30 { get; set; }
+    public string? ArtworkUrl30 { get; set; }
 
     [JsonPropertyName("artworkUrl60")]
-    public string ArtworkUrl60 { get; set; }
+    public string? ArtworkUrl60 { get; set; }
 
     [JsonPropertyName("artworkUrl100")]
-    public string ArtworkUrl100 { get; set; }
+    public string? ArtworkUrl100 { get; set; }
 
     [JsonPropertyName("collectionPrice")]
     public double CollectionPrice { get; set; }
@@ -65,10 +65,10 @@ public class SongItem : ISongItem
     public DateTimeOffset ReleaseDate { get; set; }
 
     [JsonPropertyName("collectionExplicitness")]
-    public string CollectionExplicitness { get; set; }
+    public string? CollectionExplicitness { get; set; }
 
     [JsonPropertyName("trackExplicitness")]
-    public string TrackExplicitness { get; set; }
+    public string? TrackExplicitness { get; set; }
 
     [JsonPropertyName("discCount")]
     public int DiscCount { get; set; }
@@ -86,13 +86,13 @@ public class SongItem : ISongItem
     public long TrackTimeMillis { get; set; }
 
     [JsonPropertyName("country")]
-    public string Country { get; set; }
+    public string? Country { get; set; }
 
     [JsonPropertyName("currency")]
-    public string Currency { get; set; }
+    public string? Currency { get; set; }
 
     [JsonPropertyName("primaryGenreName")]
-    public string PrimaryGenreName { get; set; }
+    public string? PrimaryGenreName { get; set; }
 
     [JsonPropertyName("isStreamable")]
     public bool IsStreamable { get; set; }
