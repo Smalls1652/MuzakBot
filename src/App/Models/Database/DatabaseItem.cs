46dfca15b2a3cbd44ca71c0fff888f13cde7954a
Timothy Small <smalls.online@icloud.com>
2024-01-09 15:46:11 -0500

    Add abstract DatabaseItem class

---
+++ src/App/Models/Database/DatabaseItem.cs
@@ -0,0 +1,19 @@
+namespace MuzakBot.App.Models.Database;
+
+/// <summary>
+/// Default properties for a database item.
+/// </summary>
+public abstract class DatabaseItem
+{
+    /// <summary>
+    /// The unique identifier for the item in the database.
+    /// </summary>
+    [JsonPropertyName("id")]
+    public string Id { get; set; } = null!;
+
+    /// <summary>
+    /// The partition key for item in the database.
+    /// </summary>
+    [JsonPropertyName("partitionKey")]
+    public string PartitionKey { get; set; } = null!;
+}
