88a2f27399afc822817822698784308138a74a4a
Timothy Small <smalls.online@icloud.com>
2024-01-08 21:17:22 -0500

    Add setup for lyrics analyzer to host startup

--- src/App/Program.cs
+++ src/App/Program.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System.Reflection;
-using MuzakBot.App.Services;
 using MuzakBot.App.Extensions;
+using MuzakBot.App.Services;
 
 var builder = Host.CreateApplicationBuilder(args);
 
@@ -52,9 +52,23 @@ builder.Services
     .AddItunesApiService()
     .AddMusicBrainzService();
 
+if (builder.Configuration.GetValue<bool>("ENABLE_LYRICS_ANALYZER"))
+{
+    builder.Services
+        .AddOpenAiService(options =>
+        {
+            options.ApiKey = builder.Configuration.GetValue<string>("OPENAI_SECRET_KEY") ?? throw new("OPENAI_SECRET_KEY is not set.");
+        })
+        .AddGeniusApiService(options =>
+        {
+            options.AccessToken = builder.Configuration.GetValue<string>("GENIUS_ACCESS_TOKEN") ?? throw new("GENIUS_ACCESS_TOKEN is not set.");
+        });
+}
+
 builder.Services.AddDiscordService(options =>
 {
     options.ClientToken = builder.Configuration.GetValue<string>("DISCORD_CLIENT_TOKEN");
+    options.EnableLyricsAnalyzer = builder.Configuration.GetValue<bool>("ENABLE_LYRICS_ANALYZER");
 
 #if DEBUG
     options.TestGuildId = builder.Configuration.GetValue<string>("DISCORD_TEST_GUILD");
