db189b833b1f72919321bee16f34cfcef9ae3fdf
Timothy Small <smalls.online@icloud.com>
2024-01-08 22:04:54 -0500

    Add code docs for OpenAiServiceActivityExtensions

--- src/App/Extensions/OpenAiServiceActivityExtensions.cs
+++ src/App/Extensions/OpenAiServiceActivityExtensions.cs
@@ -3,9 +3,28 @@ using MuzakBot.App.Models.OpenAi;
 
 namespace MuzakBot.App.Extensions;
 
+/// <summary>
+/// Extension method for <see cref="Services.OpenAiService"/> activity tracing.
+/// </summary>
 public static class OpenAiServiceActivityExtensions
 {
+    /// <summary>
+    /// Starts an activity for <see cref="Services.OpenAiService.GetLyricAnalysisAsync(string, string, string)"/>.
+    /// </summary>
+    /// <param name="activitySource">The activity source.</param>
+    /// <param name="artistName">The name of the artist.</param>
+    /// <param name="songName">The name of the song.</param>
+    /// <returns>The started activity.</returns>
     public static Activity? StartGetLyricAnalysisAsyncActivity(this ActivitySource activitySource, string artistName, string songName) => StartGetLyricAnalysisAsyncActivity(activitySource, artistName, songName, null);
+
+    /// <summary>
+    /// Starts an activity for <see cref="Services.OpenAiService.GetLyricAnalysisAsync(string, string, string)"/>.
+    /// </summary>
+    /// <param name="activitySource">The activity source.</param>
+    /// <param name="artistName">The name of the artist.</param>
+    /// <param name="songName">The name of the song.</param>
+    /// <param name="parentActivityId">The ID of the parent activity (optional).</param>
+    /// <returns>The started activity.</returns>
     public static Activity? StartGetLyricAnalysisAsyncActivity(this ActivitySource activitySource, string artistName, string songName, string? parentActivityId)
     {
         return activitySource.StartActivity(
@@ -20,6 +39,12 @@ public static class OpenAiServiceActivityExtensions
         );
     }
 
+    /// <summary>
+    /// Adds tags to an OpenAI API activity for a chat completion request.
+    /// </summary>
+    /// <param name="activity">The activity.</param>
+    /// <param name="chatCompletionRequest">The <see cref="OpenAiChatCompletionRequest"/> used in the API call.</param>
+    /// <returns>The modified activity.</returns>
     public static Activity? AddOpenAiChatCompletionRequestTags(this Activity? activity, OpenAiChatCompletionRequest chatCompletionRequest)
     {
         activity?.AddTag("openAI_request_model", chatCompletionRequest.Model);
@@ -32,6 +57,12 @@ public static class OpenAiServiceActivityExtensions
         return activity;
     }
 
+    /// <summary>
+    /// Adds tags to an OpenAI API activity for a chat completion response.
+    /// </summary>
+    /// <param name="activity">The activity.</param>
+    /// <param name="chatCompletionResponse">The <see cref="OpenAiChatCompletion"/> response from the API call.</param>
+    /// <returns>The modified activity.</returns>
     public static Activity? AddOpenAiChatCompletionResponseTags(this Activity? activity, OpenAiChatCompletion? chatCompletionResponse)
     {
         if (activity is null)
