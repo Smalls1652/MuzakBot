aca34d8511333d2762140f77af39b9d92a49037b
Timothy Small <smalls.online@icloud.com>
2024-01-08 21:54:17 -0500

    Add code docs for GeniusApiServiceActivityExtensions

--- src/App/Extensions/GeniusApiServiceActivityExtensions.cs
+++ src/App/Extensions/GeniusApiServiceActivityExtensions.cs
@@ -4,7 +4,23 @@ namespace MuzakBot.App.Extensions;
 
 internal static class GeniusApiServiceActivityExtensions
 {
+    /// <summary>
+    /// Starts an activity for <see cref="GeniusApiService.SearchAsync(string, string)"/>.
+    /// </summary>
+    /// <param name="activitySource">The activity source.</param>
+    /// <param name="artistName">The name of the artist.</param>
+    /// <param name="songName">The name of the song.</param>
+    /// <returns>The started activity.</returns>
     public static Activity? StartGeniusSearchAsyncActivity(this ActivitySource activitySource, string artistName, string songName) => StartGeniusSearchAsyncActivity(activitySource, artistName, songName, null);
+
+    /// <summary>
+    /// Starts an activity for <see cref="GeniusApiService.SearchAsync(string, string)"/>.
+    /// </summary>
+    /// <param name="activitySource">The activity source.</param>
+    /// <param name="artistName">The name of the artist.</param>
+    /// <param name="songName">The name of the song.</param>
+    /// <param name="parentActvitityId">The ID of the parent activity (optional).</param>
+    /// <returns>The started activity.</returns>
     public static Activity? StartGeniusSearchAsyncActivity(this ActivitySource activitySource, string artistName, string songName, string? parentActvitityId)
     {
         return activitySource.StartActivity(
@@ -19,7 +35,21 @@ internal static class GeniusApiServiceActivityExtensions
         );
     }
 
+    /// <summary>
+    /// Starts an activity for <see cref="GeniusApiService.GetLyricsAsync(string)"/>.
+    /// </summary>
+    /// <param name="activitySource">The activity source.</param>
+    /// <param name="url">The URL of the web page.</param>
+    /// <returns>The started activity.</returns>
     public static Activity? StartGeniusGetLyricsAsyncActivity(this ActivitySource activitySource, string url) => StartGeniusGetLyricsAsyncActivity(activitySource, url, null);
+
+    /// <summary>
+    /// Starts an activity for <see cref="GeniusApiService.GetLyricsAsync(string)"/>.
+    /// </summary>
+    /// <param name="activitySource">The activity source.</param>
+    /// <param name="url">The URL of the web page.</param>
+    /// <param name="parentActvitityId">The ID of the parent activity (optional).</param>
+    /// <returns>The started activity.</returns>
     public static Activity? StartGeniusGetLyricsAsyncActivity(this ActivitySource activitySource, string url, string? parentActvitityId)
     {
         return activitySource.StartActivity(
