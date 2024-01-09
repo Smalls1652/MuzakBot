05ed97809c7329970ec128817f11849b60f5157a
Timothy Small <smalls.online@icloud.com>
2024-01-08 22:15:06 -0500

    Add code docs for LyricsAnalyzerCommandModuleActivityExtensions

--- src/App/Extensions/LyricsAnalyzerCommandModuleActivityExtensions.cs
+++ src/App/Extensions/LyricsAnalyzerCommandModuleActivityExtensions.cs
@@ -3,8 +3,19 @@ using Discord;
 
 namespace MuzakBot.App.Extensions;
 
+/// <summary>
+/// Extension methods for activity traces used by the <see cref="LyricsAnalyzerCommandModule"/> class.
+/// </summary>
 internal static class LyricsAnalyzerCommandModuleActivityExtensions
 {
+    /// <summary>
+    /// Starts an activity for <see cref="LyricsAnalyzerCommandModule.HandleGetLyricsAsync(IInteractionContext, string, string)"/>.
+    /// </summary>
+    /// <param name="activitySource">The activity source.</param>
+    /// <param name="artistId">The ID of the artist.</param>
+    /// <param name="songId">The ID of the song.</param>
+    /// <param name="context">The <see cref="IInteractionContext"/> for the request.</param>
+    /// <returns>The started activity.</returns>
     public static Activity? StartHandleGetLyricsAsyncActivity(this ActivitySource activitySource, string artistId, string songId, IInteractionContext context)
     {
         return activitySource.StartActivity(
