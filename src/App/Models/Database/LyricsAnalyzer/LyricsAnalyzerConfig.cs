6efafb31d6c74608802f1d4a9aa2a0921cf5e55a
Timothy Small <smalls.online@icloud.com>
2024-01-10 16:17:09 -0500

    Add missing JSON property names

--- src/App/Models/Database/LyricsAnalyzer/LyricsAnalyzerConfig.cs
+++ src/App/Models/Database/LyricsAnalyzer/LyricsAnalyzerConfig.cs
@@ -6,10 +6,21 @@ public class LyricsAnalyzerConfig : DatabaseItem, ILyricsAnalyzerConfig
     public LyricsAnalyzerConfig()
     {}
 
+    [JsonPropertyName("rateLimitEnabled")]
     public bool RateLimitEnabled { get; set; }
+
+    [JsonPropertyName("rateLimitMaxRequests")]
     public int RateLimitMaxRequests { get; set; }
+
+    [JsonPropertyName("rateLimitIgnoredUserIds")]
     public ulong[]? RateLimitIgnoredUserIds { get; set; }
+
+    [JsonPropertyName("commandIsEnabledToSpecificGuilds")]
     public bool CommandIsEnabledToSpecificGuilds { get; set; }
+
+    [JsonPropertyName("commandEnabledGuildIds")]
     public ulong[]? CommandEnabledGuildIds { get; set; }
+
+    [JsonPropertyName("commandDisabledGuildIds")]
     public ulong[]? CommandDisabledGuildIds { get; set; }
 }
