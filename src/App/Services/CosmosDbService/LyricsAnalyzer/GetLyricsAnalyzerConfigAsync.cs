3383e330f4038215124729771ecf3e51d410823d
Timothy Small <smalls.online@icloud.com>
2024-01-10 18:05:12 -0500

    Add GetLyricsAnalyzerConfigAsync()

---
+++ src/App/Services/CosmosDbService/LyricsAnalyzer/GetLyricsAnalyzerConfigAsync.cs
@@ -0,0 +1,52 @@
+using System.Diagnostics;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Logging;
+using MuzakBot.App.Extensions;
+using MuzakBot.App.Models.CosmosDb;
+using MuzakBot.App.Models.Database.LyricsAnalyzer;
+
+namespace MuzakBot.App.Services;
+
+public partial class CosmosDbService
+{
+    public async Task<LyricsAnalyzerConfig> GetLyricsAnalyzerConfigAsync() => await GetLyricsAnalyzerConfigAsync(null);
+    public async Task<LyricsAnalyzerConfig> GetLyricsAnalyzerConfigAsync(string? parentActivityId)
+    {
+        //using var activity = _activitySource.StartDbGetSongLyricsItemActivity(artistName, songName, parentActivityId);
+
+        Container container = _cosmosDbClient.GetContainer(
+            databaseId: _options.DatabaseName,
+            containerId: "config"
+        );
+
+        var query = new QueryDefinition(
+            query: "SELECT * FROM c WHERE c.partitionKey = 'lyricsanalyzer-config'"
+        );
+
+        using FeedIterator feedIterator = container.GetItemQueryStreamIterator(
+            queryDefinition: query,
+            requestOptions: new()
+            {
+                PartitionKey = new PartitionKey("lyricsanalyzer-config")
+            }
+        );
+
+        using ResponseMessage response = await feedIterator.ReadNextAsync();
+
+        CosmosDbResponse<LyricsAnalyzerConfig>? cosmosDbResponse = await JsonSerializer.DeserializeAsync(
+            utf8Json: response.Content,
+            jsonTypeInfo: DatabaseJsonContext.Default.CosmosDbResponseLyricsAnalyzerConfig
+        );
+
+        if (cosmosDbResponse is null || cosmosDbResponse.Documents is null || cosmosDbResponse.Documents.Length == 0)
+        {
+            LyricsAnalyzerConfig lyricsAnalyzerConfig = new(true);
+
+            await AddOrUpdateLyricsAnalyzerConfigAsync(lyricsAnalyzerConfig);
+
+            return lyricsAnalyzerConfig;
+        }
+
+        return cosmosDbResponse.Documents.FirstOrDefault()!;
+    }
+}
