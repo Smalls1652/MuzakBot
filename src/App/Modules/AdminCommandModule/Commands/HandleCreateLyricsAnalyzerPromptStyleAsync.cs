ba8ff3fba1643ed15150d9fbcce50f5049b28a4d
Timothy Small <smalls.online@icloud.com>
2024-01-15 14:19:16 -0500

    Add prompt create/update commands and modal interactions

---
+++ src/App/Modules/AdminCommandModule/Commands/HandleCreateLyricsAnalyzerPromptStyleAsync.cs
@@ -0,0 +1,36 @@
+using Discord;
+using Discord.Interactions;
+using Microsoft.Extensions.Logging;
+using MuzakBot.App.Models.CommandModules;
+using MuzakBot.App.Models.Database.LyricsAnalyzer;
+
+namespace MuzakBot.App.Modules;
+
+public partial class AdminCommandModule
+{
+    [SlashCommand(name: "create-prompt-style", description: "Create a new prompt style.")]
+    [RequireOwner]
+    public async Task HandleCreateLyricsAnalyzerPromptStyleAsync()
+    {
+        try
+        {
+            await RespondWithModalAsync<LyricsAnalyzerPromptStyleModal>(
+                customId: "prompt-style-create"
+            );
+        }
+        catch (Exception ex)
+        {
+            await RespondAsync(
+                embeds: [
+                    new EmbedBuilder()
+                        .WithTitle("Lyrics Analyzer Prompt Style")
+                        .WithDescription($"An error occurred while creating the prompt style:\n\n{ex.Message}")
+                        .WithColor(Color.Red)
+                        .Build()
+                    ]
+            );
+
+            _logger.LogError(ex, "An error occurred while creating the prompt style: {Message}", ex.Message);
+        }
+    }
+}
---
+++ src/App/Modules/AdminCommandModule/Commands/HandleUpdateLyricsAnalyzerPromptStyleAsync.cs
@@ -0,0 +1,65 @@
+using Discord;
+using Discord.Interactions;
+using Microsoft.Extensions.Logging;
+using MuzakBot.App.Handlers;
+using MuzakBot.App.Models.CommandModules;
+using MuzakBot.App.Models.Database.LyricsAnalyzer;
+
+namespace MuzakBot.App.Modules;
+
+public partial class AdminCommandModule
+{
+    [SlashCommand(name: "update-prompt-style", description: "Update a lyrics analyzer prompt style.")]
+    [RequireOwner]
+    public async Task HandleUpdateLyricsAnalyzerPromptStyleAsync(
+        [Summary(name: "style", description: "The prompt style to edit."),
+        Autocomplete(typeof(LyricsAnalyzerPromptStyleAutoCompleteHandler))]
+        string style,
+        [Summary(name: "configType", description: "The configuration type to edit."),
+        Choice(name: "Core", value: "core"),
+        Choice(name: "User prompt", value: "user-prompt")]
+        string configType = "core"
+    )
+    {
+        LyricsAnalyzerPromptStyle? promptStyle = await _cosmosDbService.GetLyricsAnalyzerPromptStyleAsync(style);
+
+        if (promptStyle is null)
+        {
+            EmbedBuilder embed = new EmbedBuilder()
+                .WithTitle("Lyrics Analyzer Prompt Style")
+                .WithDescription($"A prompt style with the short name `{style}` does not exist.")
+                .WithColor(Color.Red);
+
+            await Context.Interaction.FollowupAsync(
+                embeds: [
+                    embed.Build()
+                    ]
+            );
+
+            return;
+        }
+
+        if (configType == "core")
+        {
+            LyricsAnalyzerPromptStyleModal promptStyleModal = new(promptStyle);
+
+            await RespondWithModalAsync(
+                customId: $"prompt-style-edit",
+                modal: promptStyleModal
+            );
+
+            return;
+        }
+
+        if (configType == "user-prompt")
+        {
+            LyricsAnalyzerPromptStyleUserPromptModal promptStyleUserPromptModal = new(promptStyle);
+            await RespondWithModalAsync(
+                customId: $"prompt-style-edit-user-prompt",
+                modal: promptStyleUserPromptModal
+            );
+
+            return;
+        }
+    }
+}
