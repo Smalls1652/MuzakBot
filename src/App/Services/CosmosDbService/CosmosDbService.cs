584267d536483883d3a9e3002f73cd368cd76205
Timothy Small <smalls.online@icloud.com>
2024-01-16 13:29:42 -0500

    Add DB initialization method

--- src/App/Services/CosmosDbService/CosmosDbService.cs
+++ src/App/Services/CosmosDbService/CosmosDbService.cs
@@ -2,6 +2,7 @@ using System.Diagnostics;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using MuzakBot.App.Logging.CosmosDb;
 
 namespace MuzakBot.App.Services;
 
@@ -26,6 +27,44 @@ public partial class CosmosDbService : ICosmosDbService
         _logger = logger;
         _options = options.Value;
         _cosmosDbClient = new CosmosClient(_options.ConnectionString);
+    }
+
+    /// <summary>
+    /// Initializes the database and containers for MuzakBot.
+    /// </summary>
+    /// <returns></returns>
+    public async Task InitializeDatabaseAsync()
+    {
+        // Initialize the database and containers for 'lyrics-analyzer',
+        // if they don't exist.
+        _logger.LogInitializeEnsureDbExists("lyrics-analyzer");
+        Database lyricsAnalyzerDb = await _cosmosDbClient.CreateDatabaseIfNotExistsAsync(
+            id: "lyrics-analyzer"
+        );
+
+        _logger.LogInitializeEnsureContainerExists("song-lyrics", "lyrics-analyzer");
+        await lyricsAnalyzerDb.CreateContainerIfNotExistsAsync(
+            id: "song-lyrics",
+            partitionKeyPath: "/partitionKey"
+        );
+
+        _logger.LogInitializeEnsureContainerExists("command-configs", "lyrics-analyzer");
+        await lyricsAnalyzerDb.CreateContainerIfNotExistsAsync(
+            id: "command-configs",
+            partitionKeyPath: "/partitionKey"
+        );
+
+        _logger.LogInitializeEnsureContainerExists("prompt-styles", "lyrics-analyzer");
+        await lyricsAnalyzerDb.CreateContainerIfNotExistsAsync(
+            id: "prompt-styles",
+            partitionKeyPath: "/partitionKey"
+        );
+
+        _logger.LogInitializeEnsureContainerExists("rate-limit", "lyrics-analyzer");
+        await lyricsAnalyzerDb.CreateContainerIfNotExistsAsync(
+            id: "rate-limit",
+            partitionKeyPath: "/partitionKey"
+        );
     }
 
     /// <inheritdoc cref="IDisposable.Dispose"/>
