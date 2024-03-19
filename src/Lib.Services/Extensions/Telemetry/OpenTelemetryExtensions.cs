9089911407b2a87f260bb4452fd70106e8925d25
Timothy Small <smalls.online@icloud.com>
2024-03-19 16:35:09 -0400

    Add extensions for OpenTelemetry setup for Lib.Services

---
+++ src/Lib.Services/Extensions/Telemetry/OpenTelemetryExtensions.cs
@@ -0,0 +1,18 @@
+using Microsoft.Extensions.Diagnostics.Metrics;
+using OpenTelemetry.Trace;
+
+namespace MuzakBot.Lib.Services.Extensions.Telemetry;
+
+public static class OpenTelemetryExtensions
+{
+    public static TracerProviderBuilder AddMuzakBotServicesTraceSources(this TracerProviderBuilder builder)
+    {
+        return builder
+            .AddSource("MuzakBot.Lib.Services.ItunesApiService")
+            .AddSource("MuzakBot.Lib.Services.MusicBrainzService")
+            .AddSource("MuzakBot.Lib.Services.OdesliService")
+            .AddSource("MuzakBot.Lib.Services.OpenAiService")
+            .AddSource("MuzakBot.Lib.Service.GeniusApiService")
+            .AddSource("MuzakBot.Lib.Services.CosmosDbService");
+    }
+}
