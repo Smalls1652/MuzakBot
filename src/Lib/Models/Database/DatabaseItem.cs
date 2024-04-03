53ab3f426f77da0d0bbf2e175fd533635a2aa335
Timothy Small <smalls.online@icloud.com>
2024-04-03 10:35:11 -0400

    Add annotations for DatabaseItem and SongLyricsItem

--- src/Lib/Models/Database/DatabaseItem.cs
+++ src/Lib/Models/Database/DatabaseItem.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace MuzakBot.Lib.Models.Database;
 
 /// <summary>
@@ -8,12 +11,15 @@ public abstract class DatabaseItem
     /// <summary>
     /// The unique identifier for the item in the database.
     /// </summary>
+    [Key]
+    [Column("id")]
     [JsonPropertyName("id")]
     public string Id { get; set; } = null!;
 
     /// <summary>
     /// The partition key for item in the database.
     /// </summary>
+    [Column("partitionKey")]
     [JsonPropertyName("partitionKey")]
     public string PartitionKey { get; set; } = null!;
 }
