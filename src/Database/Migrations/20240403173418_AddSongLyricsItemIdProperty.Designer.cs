50e891ece9511b3b6bb2008937e86f0ba177fe47
Timothy Small <smalls.online@icloud.com>
2024-04-03 13:34:54 -0400

    Add AddSongLyricsItemIdProperty DB migration

--- src/Database/Migrations/20240403173418_AddSongLyricsItemIdProperty.Designer.cs
+++ src/Database/Migrations/20240403173418_AddSongLyricsItemIdProperty.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MuzakBot.Database;
 
@@ -10,9 +11,11 @@ using MuzakBot.Database;
 namespace MuzakBot.Database.Migrations
 {
     [DbContext(typeof(SongLyricsDbContext))]
-    partial class SongLyricsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240403173418_AddSongLyricsItemIdProperty")]
+    partial class AddSongLyricsItemIdProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.3");
