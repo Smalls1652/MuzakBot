3f081cad8ce2a22b003851b959a45e6f4fd938d1
Timothy Small <smalls.online@icloud.com>
2024-04-17 13:11:52 -0400

    Add IResponse

---
+++ src/App/Models/Responses/IResponse.cs
@@ -0,0 +1,27 @@
+using Discord;
+
+namespace MuzakBot.App.Models.Responses;
+
+/// <summary>
+/// Interface for a response to a bot interaction.
+/// </summary>
+public interface IResponse
+{
+    /// <summary>
+    /// Generates the text for the response.
+    /// </summary>
+    /// <returns>The response text.</returns>
+    string GenerateText();
+
+    /// <summary>
+    /// Generates a component for the response.
+    /// </summary>
+    /// <returns>The <see cref="ComponentBuilder"/> for the response.</returns>
+    ComponentBuilder GenerateComponent();
+
+    /// <summary>
+    /// Generates an embed for the response.
+    /// </summary>
+    /// <returns>The <see cref="EmbedBuilder"/> for the response.</returns>
+    EmbedBuilder GenerateEmbed();
+}
