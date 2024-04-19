9236ba2f9265c81287014896b4cd62513cdc3dd4
Timothy Small <smalls.online@icloud.com>
2024-04-18 21:19:08 -0400

    Add LyricsAnalyzerItem

---
+++ src/Lib/Models/Database/LyricsAnalyzer/LyricsAnalyzerItem.cs
@@ -0,0 +1,62 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MuzakBot.Lib.Models.Database.LyricsAnalyzer;
+
+/// <summary>
+/// Holds data for a completed lyrics analyzer command run.
+/// </summary>
+public class LyricsAnalyzerItem : DatabaseItem
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="LyricsAnalyzerItem"/> class.
+    /// </summary>
+    [JsonConstructor()]
+    public LyricsAnalyzerItem()
+    {}
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="LyricsAnalyzerItem"/> class.
+    /// </summary>
+    /// <param name="artistName">The name of the artist.</param>
+    /// <param name="songName">The name of the song.</param>
+    /// <param name="promptStyle">The style of the prompt used for the completion.</param>
+    public LyricsAnalyzerItem(string artistName, string songName, string promptStyle)
+    {
+        Id = Guid.NewGuid().ToString();
+        PartitionKey = "lyrics-analyzer-item";
+        ArtistName = artistName;
+        SongName = songName;
+        PromptStyle = promptStyle;
+        CreatedAt = DateTimeOffset.UtcNow;
+    }
+
+    /// <summary>
+    /// The name of the artist.
+    /// </summary>
+    [Column("artistName")]
+    [JsonPropertyName("artistName")]
+    public string ArtistName { get; set; } = null!;
+
+    /// <summary>
+    /// The name of the song.
+    /// </summary>
+    [Column("songName")]
+    [JsonPropertyName("songName")]
+    public string SongName { get; set; } = null!;
+
+    /// <summary>
+    /// The style of the prompt used for the completion.
+    /// </summary>
+    [Column("promptStyle")]
+    [JsonPropertyName("promptStyle")]
+    public string PromptStyle { get; set; } = null!;
+
+    /// <summary>
+    /// The date and time the lyrics analyzer job was created.
+    /// </summary>
+    [Column("createdAt")]
+    [Timestamp]
+    [JsonPropertyName("createdAt")]
+    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+}
