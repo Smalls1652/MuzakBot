0cc71a134aa321e50df94ea3c5485f5478734227
Timothy Small <smalls.online@icloud.com>
2024-04-22 16:47:40 -0400

    Switch from CosmosDbService to EF Core for autocomplete handlers

--- src/App/Handlers/LyricsAnalyzerPromptStyleAutoCompleteHandler.cs
+++ src/App/Handlers/LyricsAnalyzerPromptStyleAutoCompleteHandler.cs
@@ -1,62 +1,66 @@
 using Discord;
 using Discord.Interactions;
-using Microsoft.Extensions.DependencyInjection;
+
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+
+using MuzakBot.Database;
 using MuzakBot.Lib.Models.Database.LyricsAnalyzer;
-using MuzakBot.Lib.Models.Itunes;
-using MuzakBot.Lib.Models.MusicBrainz;
-using MuzakBot.Lib.Services;
 
 namespace MuzakBot.App.Handlers;
 
 public class LyricsAnalyzerPromptStyleAutoCompleteHandler : AutocompleteHandler
 {
     private readonly ILogger<LyricsAnalyzerPromptStyleAutoCompleteHandler> _logger;
-    private readonly ICosmosDbService _cosmosDbService;
+    private readonly IDbContextFactory<LyricsAnalyzerDbContext> _lyricsAnalyzerDbContextFactory;
 
-    public LyricsAnalyzerPromptStyleAutoCompleteHandler(ILogger<LyricsAnalyzerPromptStyleAutoCompleteHandler> logger, ICosmosDbService cosmosDbService)
+    public LyricsAnalyzerPromptStyleAutoCompleteHandler(ILogger<LyricsAnalyzerPromptStyleAutoCompleteHandler> logger, IDbContextFactory<LyricsAnalyzerDbContext> lyricsAnalyzerDbContextFactory)
     {
         _logger = logger;
-        _cosmosDbService = cosmosDbService;
+        _lyricsAnalyzerDbContextFactory = lyricsAnalyzerDbContextFactory;
     }
 
     public override async Task<AutocompletionResult> GenerateSuggestionsAsync(IInteractionContext context, IAutocompleteInteraction autocompleteInteraction, IParameterInfo parameter, IServiceProvider services)
     {
         List<AutocompleteResult> results = new();
 
-        LyricsAnalyzerPromptStyle[] promptStyles = await _cosmosDbService.GetLyricsAnalyzerPromptStylesAsync();
+        AutocompleteOption? promptStyleInput = autocompleteInteraction.Data.Options.FirstOrDefault(item => item.Name == "style");
+
+        string? promptStyleInputValue = promptStyleInput?.Value.ToString();
+
+        using var dbContext = _lyricsAnalyzerDbContextFactory.CreateDbContext();
+
+        LyricsAnalyzerPromptStyle[] promptStyles;
+
+        if (string.IsNullOrEmpty(promptStyleInputValue))
+        {
+            promptStyles = await dbContext.LyricsAnalyzerPromptStyles
+                .AsNoTracking()
+                .WithPartitionKey("prompt-style")
+                .ToArrayAsync();
+        }
+        else
+        {
+            promptStyles = dbContext.LyricsAnalyzerPromptStyles
+                .AsNoTracking()
+                .WithPartitionKey("prompt-style")
+                .AsEnumerable()
+                .Where(item => item.Name.Contains(promptStyleInputValue, StringComparison.OrdinalIgnoreCase))
+                .ToArray();
+        }
+
         Array.Sort(promptStyles, (item1, item2) => item1.Name.CompareTo(item2.Name));
 
         _logger.LogInformation("Returned {Count} prompt styles from the database.", promptStyles.Length);
 
-        AutocompleteOption? promptStyleInput = autocompleteInteraction.Data.Options.FirstOrDefault(item => item.Name == "style");
-
-        if (promptStyleInput is null || promptStyleInput.Value is null || string.IsNullOrWhiteSpace(promptStyleInput.Value.ToString()))
+        foreach (LyricsAnalyzerPromptStyle promptStyle in promptStyles)
         {
-            _logger.LogInformation("Prompt style input was null or empty, returning all prompt styles.");
-
-            foreach (LyricsAnalyzerPromptStyle promptStyle in promptStyles)
+            results.Add(new AutocompleteResult
             {
-                results.Add(new AutocompleteResult
-                {
-                    Name = promptStyle.Name,
-                    Value = promptStyle.ShortName
-                });
-            }
-
-            return AutocompletionResult.FromSuccess(results.AsEnumerable());
+                Name = promptStyle.Name,
+                Value = promptStyle.ShortName
+            });
         }
-
-        promptStyles
-            .Where(item => item.ShortName.Contains(promptStyleInput.Value.ToString()!, StringComparison.OrdinalIgnoreCase))
-            .ToList()
-            .ForEach(item => results.Add(new AutocompleteResult
-            {
-                Name = item.Name,
-                Value = item.ShortName
-            }));
-
-        _logger.LogInformation("Filtered down to {Count} prompt styles based off the input {input}.", results.Count, promptStyleInput.Value.ToString()!);
 
         return AutocompletionResult.FromSuccess(results.AsEnumerable());
     }
