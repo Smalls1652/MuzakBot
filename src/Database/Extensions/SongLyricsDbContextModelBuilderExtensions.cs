44b21231521806f37489e6f7d8f95caaca3fccbc
Timothy Small <smalls.online@icloud.com>
2024-04-22 11:25:05 -0400

    Add ModelBuilder extension methods

--- src/Database/Extensions/SongLyricsDbContextModelBuilderExtensions.cs
+++ src/Database/Extensions/SongLyricsDbContextModelBuilderExtensions.cs
@@ -114,6 +114,11 @@ internal static class SongLyricsDbContextModelBuilderExtensions
         return modelBuilder;
     }
 
+    /// <summary>
+    /// Creates the model for the <see cref="LyricsAnalyzerItem"/> class.
+    /// </summary>
+    /// <param name="modelBuilder">The model builder.</param>
+    /// <returns>The same builder instance so that multiple calls can be chained.</returns>
     public static ModelBuilder CreateLyricsAnalyzerItemModel(this ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<LyricsAnalyzerItem>(entity =>
@@ -153,6 +158,173 @@ internal static class SongLyricsDbContextModelBuilderExtensions
                 .Property(e => e.CreatedAt)
                 .HasColumnName("createdAt")
                 .ToJsonProperty("createdAt");
+        });
+
+        return modelBuilder;
+    }
+
+    /// <summary>
+    /// Creates the model for the <see cref="LyricsAnalyzerConfig"/> class.
+    /// </summary>
+    /// <param name="modelBuilder">The model builder.</param>
+    /// <returns>The same builder instance so that multiple calls can be chained.</returns>
+    public static ModelBuilder CreateLyricsAnalyzerConfigModel(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<LyricsAnalyzerConfig>(entity =>
+        {
+            entity.ToTable("command_configs");
+            entity.ToContainer("command_configs");
+
+            entity.HasKey(e => e.Id);
+            entity.HasPartitionKey(e => e.PartitionKey);
+
+            entity
+                .Property(e => e.Id)
+                .HasColumnName("id")
+                .ToJsonProperty("id");
+
+            entity
+                .Property(e => e.PartitionKey)
+                .HasColumnName("partitionKey")
+                .ToJsonProperty("partitionKey");
+
+            entity
+                .Property(e => e.RateLimitEnabled)
+                .HasColumnName("rateLimitEnabled")
+                .ToJsonProperty("rateLimitEnabled");
+
+            entity
+                .Property(e => e.RateLimitMaxRequests)
+                .HasColumnName("rateLimitMaxRequests")
+                .ToJsonProperty("rateLimitMaxRequests");
+
+            entity
+                .Property(e => e.RateLimitIgnoredUserIds)
+                .HasColumnName("rateLimitIgnoredUserIds")
+                .ToJsonProperty("rateLimitIgnoredUserIds");
+
+            entity
+                .Property(e => e.CommandIsEnabledToSpecificGuilds)
+                .HasColumnName("commandIsEnabledToSpecificGuilds")
+                .ToJsonProperty("commandIsEnabledToSpecificGuilds");
+
+            entity
+                .Property(e => e.CommandEnabledGuildIds)
+                .HasColumnName("commandEnabledGuildIds")
+                .ToJsonProperty("commandEnabledGuildIds");
+
+            entity
+                .Property(e => e.CommandDisabledGuildIds)
+                .HasColumnName("commandDisabledGuildIds")
+                .ToJsonProperty("commandDisabledGuildIds");
+        });
+
+        return modelBuilder;
+    }
+
+    /// <summary>
+    /// Creates the model for the <see cref="LyricsAnalyzerPromptStyle"/> class.
+    /// </summary>
+    /// <param name="modelBuilder">The model builder.</param>
+    /// <returns>The same builder instance so that multiple calls can be chained.</returns>
+    public static ModelBuilder CreateLyricsAnalyzerPromptStyleModel(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<LyricsAnalyzerPromptStyle>(entity =>
+        {
+            entity.ToTable("prompt_styles");
+            entity.ToContainer("prompt_styles");
+
+            entity.HasKey(e => e.Id);
+            entity.HasPartitionKey(e => e.PartitionKey);
+
+            entity
+                .Property(e => e.Id)
+                .HasColumnName("id")
+                .ToJsonProperty("id");
+
+            entity
+                .Property(e => e.PartitionKey)
+                .HasColumnName("partitionKey")
+                .ToJsonProperty("partitionKey");
+
+            entity
+                .Property(e => e.Name)
+                .HasColumnName("name")
+                .ToJsonProperty("name");
+
+            entity
+                .Property(e => e.ShortName)
+                .HasColumnName("shortName")
+                .ToJsonProperty("shortName");
+
+            entity
+                .Property(e => e.AnalysisType)
+                .HasColumnName("analysisType")
+                .ToJsonProperty("analysisType");
+
+            entity
+                .Property(e => e.Prompt)
+                .HasColumnName("prompt")
+                .ToJsonProperty("prompt");
+
+            entity
+                .Property(e => e.NoticeText)
+                .HasColumnName("noticeText")
+                .ToJsonProperty("noticeText");
+
+            entity
+                .Property(e => e.CreatedOn)
+                .HasColumnName("createdOn")
+                .ToJsonProperty("createdOn");
+
+            entity
+                .Property(e => e.LastUpdated)
+                .HasColumnName("lastUpdated")
+                .ToJsonProperty("lastUpdated");
+        });
+
+        return modelBuilder;
+    }
+
+    /// <summary>
+    /// Creates the model for the <see cref="LyricsAnalyzerUserRateLimit"/> class.
+    /// </summary>
+    /// <param name="modelBuilder">The model builder.</param>
+    /// <returns>The same builder instance so that multiple calls can be chained.</returns>
+    public static ModelBuilder CreateLyricsAnalyzerUserRateLimitModel(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<LyricsAnalyzerUserRateLimit>(entity =>
+        {
+            entity.ToTable("user_rate_limit");
+            entity.ToContainer("user_rate_limit");
+
+            entity.HasKey(e => e.Id);
+            entity.HasPartitionKey(e => e.PartitionKey);
+
+            entity
+                .Property(e => e.Id)
+                .HasColumnName("id")
+                .ToJsonProperty("id");
+
+            entity
+                .Property(e => e.PartitionKey)
+                .HasColumnName("partitionKey")
+                .ToJsonProperty("partitionKey");
+
+            entity
+                .Property(e => e.UserId)
+                .HasColumnName("userId")
+                .ToJsonProperty("userId");
+
+            entity
+                .Property(e => e.CurrentRequestCount)
+                .HasColumnName("currentRequestCount")
+                .ToJsonProperty("currentRequestCount");
+
+            entity
+                .Property(e => e.LastRequestTime)
+                .HasColumnName("lastRequestTime")
+                .ToJsonProperty("lastRequestTime");
         });
 
         return modelBuilder;
