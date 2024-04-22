362e0f8a6f7ed395ae271717cf56afcb2bc02274
Timothy Small <smalls.online@icloud.com>
2024-04-22 15:05:42 -0400

    Add methods for updating existing items with a Discriminator property

--- src/Database/Extensions/LyricsAnalyzerDbContextExtensions.cs
+++ src/Database/Extensions/LyricsAnalyzerDbContextExtensions.cs
@@ -1,8 +1,14 @@
+using System.Text;
+using System.Text.Json.Nodes;
+
+using Microsoft.Azure.Cosmos;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 using MuzakBot.Database.Models;
+using MuzakBot.Lib.Models.Database;
 
 namespace MuzakBot.Database.Extensions;
 
@@ -84,9 +90,9 @@ public static class LyricsAnalyzerDbContextExtensions
     {
         using var scope = host.Services.CreateScope();
 
-        var db = scope.ServiceProvider.GetRequiredService<IDbContextFactory<LyricsAnalyzerDbContext>>();
+        var dbContextFactory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<LyricsAnalyzerDbContext>>();
 
-        using var context = db.CreateDbContext();
+        using var context = dbContextFactory.CreateDbContext();
 
         if (context.Database.IsRelational())
         {
@@ -97,6 +103,157 @@ public static class LyricsAnalyzerDbContextExtensions
         if (context.Database.IsCosmos())
         {
             await context.Database.EnsureCreatedAsync();
+
+            DatabaseUpdate databaseUpdates = await context.DatabaseUpdates
+                .WithPartitionKey("applied-updates")
+                .FirstOrDefaultAsync()
+            ?? new()
+            {
+                Id = Guid.NewGuid().ToString(),
+                PartitionKey = "applied-updates",
+                FirstRun = true,
+                MigratedToEfCore = false
+            };
+
+            if (!databaseUpdates.MigratedToEfCore)
+            {
+                using ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+                {
+                    builder.AddSimpleConsole(options =>
+                    {
+                        options.IncludeScopes = true;
+                    });
+                });
+
+                ILogger logger = loggerFactory.CreateLogger("MuzakBot.Database.Upgrade");
+
+                logger.LogWarning("Initial EF Core migration has not been applied yet. Applying migration...");
+
+                await dbContextFactory.EnsureDiscriminatorValuesExistAsync_CosmosDb();
+
+                databaseUpdates.MigratedToEfCore = true;
+
+                if (databaseUpdates.FirstRun)
+                {
+                    context.DatabaseUpdates.Add(databaseUpdates);
+                }
+                else
+                {
+                    context.DatabaseUpdates.Update(databaseUpdates);
+                }
+
+                await context.SaveChangesAsync();
+
+                logger.LogInformation("Initial EF Core migration has been applied.");
+            }
+        }
+    }
+
+    /// <summary>
+    /// Ensures that discriminator values exist in the Cosmos DB containers.
+    /// </summary>
+    /// <param name="dbContextFactory">The database context factory.</param>
+    /// <returns></returns>
+    private static async Task EnsureDiscriminatorValuesExistAsync_CosmosDb(this IDbContextFactory<LyricsAnalyzerDbContext> dbContextFactory)
+    {
+        Tuple<string, string, string>[] containerUpdates = [
+            new("command_configs", "lyricsanalyzer-config", "LyricsAnalyzerConfig"),
+            new("prompt_styles", "prompt-style", "LyricsAnalyzerPromptStyle"),
+            new("user_rate_limit", "user-item", "LyricsAnalyzerUserRateLimit")
+        ];
+
+        foreach (var (containerName, partitionKey, discriminatorValue) in containerUpdates)
+        {
+            await dbContextFactory.AddDiscriminatorPropertyAsync(
+                databaseName: "lyrics_analyzer",
+                containerName: containerName,
+                partitionKey: partitionKey,
+                discriminatorValue: discriminatorValue
+            );
+        }
+    }
+
+    /// <summary>
+    /// Adds a discriminator property to documents in a Cosmos DB container.
+    /// </summary>
+    /// <param name="dbContextFactory">The database context factory.</param>
+    /// <param name="databaseName">The name of the database.</param>
+    /// <param name="containerName">The name of the container.</param>
+    /// <param name="partitionKey">The partition key for the items in the container.</param>
+    /// <param name="discriminatorValue">The value of the discriminator property.</param>
+    /// <returns></returns>
+    private static async Task AddDiscriminatorPropertyAsync(this IDbContextFactory<LyricsAnalyzerDbContext> dbContextFactory, string databaseName, string containerName, string partitionKey, string discriminatorValue)
+    {
+        using ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.AddSimpleConsole(options =>
+            {
+                options.IncludeScopes = true;
+            });
+        });
+
+        ILogger logger = loggerFactory.CreateLogger("MuzakBot.Database.Upgrade");
+
+        using var dbContext = dbContextFactory.CreateDbContext();
+
+        var cosmosClient = dbContext.Database.GetCosmosClient();
+
+        var container = cosmosClient.GetContainer(databaseName, containerName);
+
+        QueryDefinition query = new("SELECT * FROM c");
+
+        using var feedIterator = container.GetItemQueryStreamIterator(
+            queryDefinition: query,
+            requestOptions: new()
+            {
+                PartitionKey = new(partitionKey)
+            }
+        );
+
+        while (feedIterator.HasMoreResults)
+        {
+            using var response = await feedIterator.ReadNextAsync();
+
+            JsonNode? jsonNode = JsonNode.Parse(
+                utf8Json: response.Content
+            );
+
+            if (jsonNode is null)
+            {
+                continue;
+            }
+
+            if (jsonNode["Documents"] is null)
+            {
+                continue;
+            }
+
+            foreach (var document in jsonNode["Documents"]!.AsArray())
+            {
+                if (document is null)
+                {
+                    continue;
+                }
+
+                if (document["Discriminator"] is not null)
+                {
+                    continue;
+                }
+
+                document["Discriminator"] = JsonValue.Create(discriminatorValue);
+
+                using var stream = new MemoryStream();
+
+                await stream.WriteAsync(Encoding.UTF8.GetBytes(document.ToJsonString()));
+
+                await container.ReplaceItemStreamAsync(
+                    streamPayload: stream,
+                    id: document["id"]!.GetValue<string>(),
+                    partitionKey: new(partitionKey)
+                );
+            }
+
+            logger.LogInformation("Discriminator property added to documents in container '{ContainerName}'.", containerName);
         }
     }
 }
