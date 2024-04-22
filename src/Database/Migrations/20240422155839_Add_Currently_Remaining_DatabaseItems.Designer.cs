6959455f39c69553c1405064eeb9f465ad4d56cd
Timothy Small <smalls.online@icloud.com>
2024-04-22 12:00:25 -0400

    Add migrations for the new items added

--- src/Database/Migrations/20240422155839_Add_Currently_Remaining_DatabaseItems.Designer.cs
+++ src/Database/Migrations/20240422155839_Add_Currently_Remaining_DatabaseItems.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MuzakBot.Database;
 
@@ -10,9 +11,11 @@ using MuzakBot.Database;
 namespace MuzakBot.Database.Migrations
 {
     [DbContext(typeof(LyricsAnalyzerDbContext))]
-    partial class LyricsAnalyzerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240422155839_Add_Currently_Remaining_DatabaseItems")]
+    partial class Add_Currently_Remaining_DatabaseItems
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.4");
