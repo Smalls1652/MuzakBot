702ce938fb668bc6ade2e34a188baa38325fa25a
Timothy Small <smalls.online@icloud.com>
2024-05-02 22:12:02 -0400

    Add padding to the container

--- src/WebApp/tailwind.config.js
+++ src/WebApp/tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+  "content": [
     "./**/*.{razor,cshtml,html}"
   ],
-  theme: {
-    extend: {},
+  "theme": {
+    "container": {
+      "padding": {
+        "DEFAULT": "1rem",
+        "sm": "2rem",
+        "lg": "7rem",
+        "xl": "8rem",
+        "2xl": "8rem"
+      }
+    },
+    "extend": {}
   },
-  plugins: []
+  "plugins": []
 }
 
