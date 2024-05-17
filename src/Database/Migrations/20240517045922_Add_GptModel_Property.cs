0fe2dcd7807906910329a763162fbbd1cf08f949
Timothy Small <smalls.online@icloud.com>
2024-05-17 01:17:38 -0400

    Add GptModel property to LyricsAnalyzerPromptStyle

---
+++ src/Database/Migrations/20240517045922_Add_GptModel_Property.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MuzakBot.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_GptModel_Property : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "gptModel",
+                table: "prompt_styles",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "gptModel",
+                table: "prompt_styles");
+        }
+    }
+}
